ca2e6fe3cf6094ec9558437eabe741f5c9f680f1
SofiaDani <sofia.dsmachado@gmail.com>
2022-04-19 23:29:11 +0100

    dictionary form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer-text">
+          <small>Coded by Sofia Machado</small>
+        </footer>
+      </div>
     </div>
   );
 }
